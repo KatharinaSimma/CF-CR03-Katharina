8202b525a7d56f754f4cd7e8aecb35bc89e572c1
Katharina Simma <katharina.simma@diebin.at>
2020-09-26 14:00:32 +0200

    finish JS, finish core project

--- js/script.js
+++ js/script.js
@@ -1,28 +1,10 @@
-
-// First Safe Input
-/*var yourName = document.getElementById("yourName").value;
-console.log(yourName);
-console.log(typeof document.getElementById("yourName").value);
-
-//var age = Number(document.getElementById("age").value);
-//console.log(age);
-console.log(typeof age);
-
-var country = document.getElementById("country").value;
-console.log(country);
-console.log(typeof country);
-
-var horses = Number(document.getElementById("horses").value);
-console.log(horses);
-console.log(typeof horses);*/
-
-//var yourName = "Kathi"
-//var age = 33
-//var country = "Greece"
-//var horses = 172
+// I wrote my function as tightly as possible
+/* I assume I will only use the data from the form field for this specific 
+function so I did not declare variable outside of the function.*/
 
 // Function creating Output
 function insurance(){
+// part #1  picks up values and calculates accordingly
     if (document.getElementById("country").value == "Austria"){
         insurance = Number(document.getElementById("horses").value)*100/Number(document.getElementById("age").value)+50;
     } else if (document.getElementById("country").value == "Hungary"){
@@ -30,10 +12,13 @@ function insurance(){
     } else if (document.getElementById("country").value == "Greece"){
         insurance = Number(document.getElementById("horses").value)*150/(Number(document.getElementById("age").value)+3)+50;
     }
+// part #2 tests user input and throws an error message if need be
     insuranceOffer = insurance.toFixed(2);
-    
-    document.getElementById("result").innerHTML = document.getElementById("yourName").value + ", your insurance costs " +  insuranceOffer +  "€! "
+        if (isNaN(insuranceOffer) == true || document.getElementById("yourName").value != true){
+            document.getElementById("result").innerHTML = "Please fill in all fields correctly to get a quote!"
+        } else {
+            document.getElementById("result").innerHTML = document.getElementById("yourName").value + ", your insurance costs " +  insuranceOffer +  "€! "}
 }
 
-// Call Function by Event
+// Call Function as by event
 document.getElementById("button").addEventListener("click",insurance,false);
